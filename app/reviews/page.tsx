ef56b224bd77f722819f48cd152f69d1eb7c590c
VanessaRodrigues <vanessa.rodrigues@e-sign.co.uk>
2024-03-05 08:46:29 -0300

    Remove unused components

--- app/reviews/page.tsx
+++ app/reviews/page.tsx
@@ -1,12 +1,10 @@
 // pages/reviews/index.tsx
 import React from 'react'
-import ShareLinkButton from '../../components/ShareLinkButton'
 
 const ReviewsPage: React.FC = () => {
   return (
     <>
       <h1>Starting Reviews Page</h1>
-      <ShareLinkButton />
     </>
   )
 }
